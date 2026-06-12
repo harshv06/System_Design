c840978080a06f0bbf2d469752b6d22329edf04f
harshv06 <vishwakarmah45@gmail.com>
2026-06-12 13:34:00 +0530

    Add: code with eager loading for singleton pattern

---
+++ Singleton_Pattern/code.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Singleton{
+    private:
+        static Singleton* instance;
+
+    public:
+        static Singleton* getInstance(){
+            return instance;
+        }
+};
+
+Singleton* Singleton::instance=new Singleton();
+
+int main(){
+    Singleton* s1=Singleton::getInstance();
+    Singleton* s2=Singleton::getInstance();
+    cout<<(s1 == s2)<<endl;
+}
